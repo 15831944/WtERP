29bfd17f50add652a80f9ce1a019b38e87a9cc6c
Saif <saifi0102@gmail.com>
2018-06-21 14:39:54 +0500

    Moved TODO list to Docs folder

--- src/ERP/Main.cpp
+++ src/ERP/Main.cpp
@@ -2,27 +2,6 @@
 #include "Application/WServer.h"
 #include "Application/WApplication.h"
 
-//TODO: Create indexes
-//TODO: Check exception handling
-//TODO: Check for shared_ptr and Dbo::ptr cyclical circular references
-//TODO: Upgrade Font Awesome
-
-//TODO: Record deletion
-//TODO: Make Location-Entity a ManyToMany relation
-//TODO: Fix LOG_ERROR(Wt::Dbo::ptr) useless errors
-//TODO: Look into QueryModel exception handling
-
-//TODO: Follow Multiple Dialogs error report
-//TODO: Follow MySQL backend error report
-//TODO: Follow WMenuItem::setContents() error report
-
-//TODO: Increase precision of Payment::Money data type
-//TODO: Account Entry value must be > 0
-//TODO: Localize DateTimes
-//TODO: viewName() and viewTitle()
-//TODO: viewName() for record not found
-//TODO: Create abstraction for list proxy models
-
 int main(int argc, char *argv[])
 {
 	/* *************************************************************************
